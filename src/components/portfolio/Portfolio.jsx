ee644629417cfc4bb0ff9413688e05563fe21e61
eccentricality <youngmin13@gmail.com>
2021-09-18 16:32:57 -0400

    containers given body and color

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./portfolio.scss"
+
+export default function Portfolio() {
+    return (
+        <div>
+            <div className="portfolio"></div>
+        </div>
+    )
+}
